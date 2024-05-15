9930c2057613fd41c666177923bb14985c7a9aaa
cuddiale <alessiocuddretto@hotmail.com>
2024-05-15 17:44:56 +0200

    update

--- script.js
+++ script.js
@@ -2,14 +2,22 @@ const character = "#";
 const count = 8;
 const rows = [];
 
-for (let i = 0; i < count; i = i + 1) {
-  rows.push(i);
+function padRow(rowNumber, rowCount) {
+  return (
+    " ".repeat(rowCount - rowNumber) +
+    character.repeat(2 * rowNumber - 1) +
+    " ".repeat(rowCount - rowNumber)
+  );
+}
+
+for (let i = 2; i < count; i++) {
+  rows.push(padRow(i + 1, count));
 }
 
 let result = "";
 
 for (const row of rows) {
-  result = result + row;
+  result = result + "\n" + row;
 }
 
 console.log(result);
